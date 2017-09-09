e559cbd6bbfd190058f657eefbc2465278883168
Zii <dczii@live.com>
2017-09-09 17:55:45 -0600

    Change weather API.

--- js/index.js
+++ js/index.js
@@ -1,54 +1,52 @@
 window.onload = function () {
     document.getElementById("icon1").style.display = 'none';
-	navigator.geolocation.getCurrentPosition(getCurrentLocation);
+    getLocation("Metric");
     setIcon();
 }
 
-let lat = 0;
-let long = 0
-function getLocation (param) {
+var temp = "Metric";
+var defaultCity = ""
+
+function getLocation (temp) {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(getCurrentLocation);
+		$.get("https://ipapi.co/json", function(data) {
+            defaultCity = data.city
+            getWeather(data.city, temp);
+        });
     } else { 
         document.getElementById("weather").innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function getCurrentLocation (position) {
-	let currentPosition;
-    currentPosition = position;
-    lat = currentPosition.coords.latitude;
-    long = currentPosition.coords.longitude;
-    
-	callJSON("metric")
-}
-
-function callJSON (val) {
-    let unit = val;
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=" + unit + "&APPID=b231606340553d9174136f7f083904b3", function(data) {
-        let rawJson = JSON.stringify(data);
-        let json = JSON.parse(rawJson);
-        updateWeather(json); //Update Weather parameters
-    });
+function getWeather (city, temp) {
+	var api = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=";
+    var units = "&units=" + temp;
+    var appid = "&APPID=061f24cf3cde2f60644a8240302983f2";
+    var $http = api + city + units + appid;
+    $.getJSON($http, function(data) {
+        updateWeather(data);
+    });   
 }
 
 function updateWeather (position) {
     //set Icon
     document.getElementById("icon1").style.display = 'block';
+    document.getElementById("city").style.display = 'block';
     document.getElementById("icon2").style.display = 'none';
     setIcon()
     //Set weather name and temp
-    document.getElementById("weather_temp").innerHTML = position.main.temp;
-	document.getElementById("weather_city").innerHTML = position.name + ", " + position.sys.country;
+    document.getElementById("weather_temp").innerHTML = Math.round(position.main.temp);
+    document.getElementById("weather_city").innerHTML = position.name + ", " + position.sys.country;
+    document.getElementById("weather_city").value = position.name + ", " + position.sys.country;
     //Set Icon Weather
     let weather = position.weather[0].description;
     if(weather.indexOf("rain") >= 0) {
         setIcon(Skycons.RAIN)
-        // skycons.set("animated-icon", Skycons.RAIN);
+        document.body.style.background = "#616161 url('https://wallpapercave.com/wp/wxHBbSq.jpg') no-repeat";
     }
     else if (weather.indexOf("sunny") >= 0) {
         setIcon(Skycons.CLEAR_DAY)
-        // skycons.set("animated-icon", Skycons.CLEAR_DAY);
+        document.body.style.background = "#616161 url('http://7-themes.com/data_images/collection/9/4505433-sunny-day-wallpapers.jpg') no-repeat";
     }
 }
 
@@ -65,19 +63,27 @@ function setIcon(val) {
 
 function changeTemp(param) {
     if(param.id == "tempF") {
+        temp = "Imperial";
         document.getElementById(param.id).style.fontSize = "1.8em";
         document.getElementById("tempC").style.fontSize = "1em";
-        callJSON("Imperial")
+        getWeather(defaultCity, temp);
     }
     else if (param.id == "tempC") {
+        temp = "Metric";
         document.getElementById(param.id).style.fontSize = "1.8em";
         document.getElementById("tempF").style.fontSize = "1em";
-        callJSON("Metric")
+        getWeather(defaultCity, temp)
     }
 } 
 
 
+$(document).ready(function() {
+    $("#location").on("click", function() {
+        defaultCity = $('#city').val() 
+        getWeather(defaultCity, temp)
+    });
 
+});
 
 
 
